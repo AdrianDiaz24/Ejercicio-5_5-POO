1c74968dc0b5389b31b83ec4eb76f12abde42d0e
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2025-02-21 11:33:07 +0100

    1.0

--- src/Usuario.kt
+++ src/Usuario.kt
@@ -1,15 +1,14 @@
 sealed class Usuario{
 
-    data class Profesor(val id: String, val nombre: String, val departamento: String)
-    data class Estudiante(val id: String, val nombre: String, val carrera: String)
-    data class Visitante(val id: String, val nombre: String)
+    data class Profesor(val id: String, val nombre: String, val departamento: String): Usuario()
+    data class Estudiante(val id: String, val nombre: String, val carrera: String): Usuario()
+    data class Visitante(val id: String, val nombre: String): Usuario()
 
     fun pedirLibro(usuario: Usuario, libro: Libro){
         when (usuario) {
-            is Usuario.Profesor -> println("Libro: ${libro.titulo} alquilado 30 dias")
-            is Usuario.Estudiante -> println("Libro: ${libro.titulo} alquilado 15 dias")
-            is Usuario.Visitante -> println("Un visitante no puede alquilar libros")
-            else -> {}
+            is Profesor -> println("Libro: ${libro.titulo} alquilado 30 dias")
+            is Estudiante -> println("Libro: ${libro.titulo} alquilado 15 dias")
+            is Visitante -> println("Un visitante no puede alquilar libros")
         }
     }
 
